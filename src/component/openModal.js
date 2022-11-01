c38a555d51e753d3a21f0df8f46daa8b0929f7c6
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2022-11-01 09:50:14 -0500

    project-moduel

---
+++ src/component/openModal.js
@@ -0,0 +1,18 @@
+//funcion tradicional
+import React, {Suspense, lazy} from "react";
+import {createRoot} from "react-dom/client";
+export default function openModal(){
+const Modal = lazy(()=> import("./modal"));
+const modalDiv = document.createElement("div");
+modalDiv.id="modal";
+document.body.appendChild(modalDiv);
+
+const root = createRoot(modalDiv);
+root.render(
+<Suspense fallback={<div>Loading...</div>}>
+<Modal root={root} title="Modal de prueba">
+    Contenido del Modal
+    </Modal>    
+</Suspense>
+)
+}
